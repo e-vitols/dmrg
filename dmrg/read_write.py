ef2a2a781eeb1916695c848b9de9b42b08ecf026
Erik Vitols <erik.vitols@proton.me>
2025-10-27 12:45:53 +0100

    naive implementation of read/write h5py files to/from numpy arrays

--- dmrg/read_write.py
+++ dmrg/read_write.py
@@ -1,1 +1,11 @@
-# placeholder
+import numpy as np
+import h5py
+
+def read_h5_to_numpy(file):
+    f = h5py.File(file, 'r')
+    return f
+
+def write_to_h5(file, input_data):
+    f = h5py.File(file, "w")
+    f.create_dataset('data', data=input_data)
+    f.close()
