707f7276394bf3d53e360d6569c2438fa055cff6
Erik Vitols <erik.vitols@proton.me>
2025-10-27 14:17:35 +0100

    minor update to read/write array to hdf5 with some comments

--- dmrg/read_write.py
+++ dmrg/read_write.py
@@ -1,11 +1,16 @@
 import numpy as np
 import h5py
 
-def read_h5_to_numpy(file):
-    f = h5py.File(file, 'r')
-    return f
+def load_h5_to_array(file):
+    """
+    Load a numpy array to hdf5 file
+    """
+    with h5py.File(file, 'r') as f:
+        return f['array_data'][:]
 
-def write_to_h5(file, input_data):
-    f = h5py.File(file, "w")
-    f.create_dataset('data', data=input_data)
-    f.close()
+def write_array_to_h5(file, input_data):
+    """
+    Save a numpy array to hdf5 file
+    """
+    with h5py.File(file, 'w') as f:
+        f.create_dataset('array_data', data=input_data)
