29a8255196194cc885123dbf7bb29ecc53f3e1e6
Erik Vitols <erik.vitols@proton.me>
2025-11-24 12:58:45 +0100

    added initial (very simple) test for canonical form

---
+++ tests/test_canonical_form.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+
+import dmrg
+from dmrg import mps
+from dmrg.mps import MatrixProductState
+
+class TestCanonicalize:
+
+    def test_canonicalize(self,local_dim=4, m_bonddim=8, nr_sites=6):
+        #tt = mps.MatrixProductState()
+        tt = MatrixProductState()
+        tt.local_dim = local_dim
+        tt.max_bond_dim = m_bonddim
+        tt.nr_sites = nr_sites
+        tt._initialize_random_mps()
+        mps = tt.mps
+        tt.canonicalize_mps(6)
+
+        orthonorm = np.einsum('ldr, rdl -> lr', tt.mps[0], tt.mps[0].conjugate())
+
+        assert np.max(np.abs(orthonorm - np.eye(4))) < 1e-10
