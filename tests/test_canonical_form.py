a0d23dfbd3be3f8eb2e6de187ff190be5fdaed82
Erik Vitols <erik.vitols@proton.me>
2025-11-27 09:02:16 +0100

    updated the test slightly to handle both left and right canonicalization

--- tests/test_canonical_form.py
+++ tests/test_canonical_form.py
@@ -1,14 +1,14 @@
-import pytest
 import numpy as np
+import pytest
 
 import dmrg
 from dmrg import mps
 from dmrg.mps import MatrixProductState
 
-class TestCanonicalize:
 
-    def test_canonicalize(self,local_dim=4, m_bonddim=8, nr_sites=6):
-        #tt = mps.MatrixProductState()
+class TestCanonicalize:
+    def test_right_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
+        # tt = mps.MatrixProductState()
         tt = MatrixProductState()
         tt.local_dim = local_dim
         tt.max_bond_dim = m_bonddim
@@ -17,6 +17,20 @@ class TestCanonicalize:
         mps = tt.mps
         tt.canonicalize_mps(6)
 
-        orthonorm = np.einsum('ldr, rdl -> lr', tt.mps[0], tt.mps[0].conjugate())
+        orthonorm = np.einsum("ldr, rdl -> lr", tt.mps[0], tt.mps[0].conjugate())
+
+        assert np.max(np.abs(orthonorm - np.eye(4))) < 1e-10
+
+    def test_left_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
+        # tt = mps.MatrixProductState()
+        tt = MatrixProductState()
+        tt.local_dim = local_dim
+        tt.max_bond_dim = m_bonddim
+        tt.nr_sites = nr_sites
+        tt._initialize_random_mps()
+        mps = tt.mps
+        tt.canonicalize_mps(0)
+
+        orthonorm = np.einsum("ldr, rdl -> lr", tt.mps[-1], tt.mps[-1].conjugate())
 
         assert np.max(np.abs(orthonorm - np.eye(4))) < 1e-10
