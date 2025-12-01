9764881ca375a9fc45152cf0dd10cdc0a30bdceb
Erik Vitols <erik.vitols@proton.me>
2025-12-01 01:33:35 +0100

    extended to test also for the invariance of the norm under canonicalization

--- tests/test_canonical_form.py
+++ tests/test_canonical_form.py
@@ -7,7 +7,7 @@ from dmrg.mps import MatrixProductState
 
 
 class TestCanonicalize:
-    def test_right_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
+    def test_left_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
         # tt = mps.MatrixProductState()
         tt = MatrixProductState()
         tt.local_dim = local_dim
@@ -17,11 +17,11 @@ class TestCanonicalize:
         mps = tt.mps
         tt.canonicalize_mps(6)
 
-        orthonorm = np.einsum("ldr, rdl -> lr", tt.mps[0], tt.mps[0].conjugate())
+        orthonorm = np.einsum("ldr, ldR -> rR", tt.mps[0], tt.mps[0].conjugate())
 
         assert np.max(np.abs(orthonorm - np.eye(4))) < 1e-10
 
-    def test_left_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
+    def test_right_canonicalize(self, local_dim=4, m_bonddim=8, nr_sites=6):
         # tt = mps.MatrixProductState()
         tt = MatrixProductState()
         tt.local_dim = local_dim
@@ -29,8 +29,32 @@ class TestCanonicalize:
         tt.nr_sites = nr_sites
         tt._initialize_random_mps()
         mps = tt.mps
-        tt.canonicalize_mps(0)
 
-        orthonorm = np.einsum("ldr, rdl -> lr", tt.mps[-1], tt.mps[-1].conjugate())
+        env = np.array([[1.0]], dtype=complex)
+        for A in mps:
+            env = np.einsum("lL, ldr, LdR -> rR", env, A, A.conjugate())
+        norm_before = float(env.squeeze().real)
+
+        tt.canonicalize_mps(0)
+        mps = tt.mps
+
+        # orthonorm = np.einsum("ldr, rdl -> lr", tt.mps[-1], tt.mps[-1].conjugate())
+        orthonorm = np.einsum("ldr, Ldr -> lL", tt.mps[-1], tt.mps[-1].conjugate())
 
         assert np.max(np.abs(orthonorm - np.eye(4))) < 1e-10
+
+        env = np.array([[1.0]], dtype=complex)
+
+        for A in mps:
+            env = np.einsum("lL, ldr, LdR -> rR", env, A, A.conjugate())
+        # assert env.squeeze().imag < 1e-12
+
+        norm = float(env.squeeze().real)
+
+        rel_err = abs(norm - norm_before) / norm_before
+        assert rel_err < 1e-10
+        # assert abs(norm - norm_before) < 1e-4
+        canonical_norm = np.einsum("ldr, ldr", mps[0], mps[0].conjugate())
+        rel_err = abs(canonical_norm - norm_before) / norm_before
+
+        assert rel_err < 1e-10
